d9eba2fd92bbcee168ed612851eb10d2cc7d2a07
Vivekanand P V <vivekanandpv@outlook.com>
2023-02-16 11:17:06 +0530

    corrected primary key conventionally

--- Models/User.cs
+++ Models/User.cs
@@ -1,7 +1,7 @@
 ﻿namespace AspNetCore6.BugTracker.Models;
 
 public class User {
-    public int Id { get; set; }
+    public int UserId { get; set; }
     public string Username { get; set; }
     public string FullName { get; set; }
     public string AvatarUrl { get; set; }
