0e2f9310f723a326498e61276f1ff0ae63fd1c73
Vivekanand P V <vivekanandpv@outlook.com>
2023-02-16 12:19:57 +0530

    done

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using AspNetCore6.BugTracker.Filters;
 using AspNetCore6.BugTracker.Services.Implementations;
 using AspNetCore6.BugTracker.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace AspNetCore6.BugTracker
 {
@@ -25,7 +26,14 @@ namespace AspNetCore6.BugTracker
             builder.Services.AddScoped<IBugService, BugService>();
             builder.Services.AddScoped<IMessageService, MessageService>();
 
+            builder.Services.AddSwaggerGen(config => { config.SwaggerDoc("v1.0.0", new OpenApiInfo { Title = "BugTracker API Documentation" }); });
+
             var app = builder.Build();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(config => {
+                config.SwaggerEndpoint("/swagger/v1.0.0/swagger.json", "BugTracker");
+            });
 
             app.MapControllers();
 
